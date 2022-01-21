4c49b3c5f52c940459c643e5aa5500245028fa2f
finn-01 <nguyensam576@gmail.com>
2022-01-21 15:10:10 +0700

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<nav className="navbar">
+			<div className="nav-center">
+				<Link to="/">
+					<h2 className="focus-in-contract-bck">LOGO</h2>
+				</Link>
+				<ul className="nav-links">
+					<li className="wobble-hor-bottom">
+						<Link to="/">HOME</Link>
+					</li>
+					<li className="wobble-hor-bottom">
+						<Link to="/about">ABOUT</Link>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
