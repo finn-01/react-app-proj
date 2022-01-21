2cde293a716c06256dca57f88b3fba016abda669
finn-01 <nguyensam576@gmail.com>
2022-01-21 15:35:15 +0700

    context.js

--- src/context.js
+++ src/context.js
@@ -8,6 +8,46 @@ const AppProvider = ({ children }) => {
 	const [searchTerm, setSearchTerm] = useState("");
 	const [cocktails, setCocktails] = useState([]);
 
+	const fetchData = async () => {
+		try {
+			//Lay du lieu
+			const response = await fetch(`${url}${searchTerm}`);
+			const data = await response.json();
+			//console.log(data);
+
+			const { drinks } = data;
+
+			//Neu co
+			if (drinks) {
+				const newCocktail = drinks.map((item) => {
+					const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } =
+						item;
+					return {
+						id: idDrink,
+						name: strDrink,
+						img: strDrinkThumb,
+						info: strAlcoholic,
+						glass: strGlass,
+					};
+				});
+
+				//set cocktails
+				setCocktails(newCocktail);
+			} else {
+				setCocktails([]);
+			}
+
+			setLoading(false);
+		} catch (error) {
+			console.log(error);
+			setLoading(false);
+		}
+	};
+
+	useEffect(() => {
+		fetchData();
+	}, [searchTerm]);
+
 	return <AppContext.Provider value="valus">{children}</AppContext.Provider>;
 };
 export const useGlobalContext = () => {
